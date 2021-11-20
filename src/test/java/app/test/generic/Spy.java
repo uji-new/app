fe36b8c05b54af68550f3335147c4d100512d2ac
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-11-20 20:45:49 +0100

    spy persistance layer

--- src/test/java/app/test/generic/Spy.java
+++ src/test/java/app/test/generic/Spy.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Service;
 import app.api.service.EventsService;
 import app.api.service.NewsService;
 import app.api.service.WeatherService;
+import app.manager.AccountManager;
 import app.manager.QueryManager;
 
 @Service
 public class Spy {
+    @SpyBean public AccountManager accountManager;
     @SpyBean public QueryManager queryManager;
     @SpyBean public WeatherService weatherService;
     @SpyBean public EventsService eventsService;
