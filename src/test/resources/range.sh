b66389e065423997ee05152ffdfab8383323a13b
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-11-26 01:02:44 +0100

    smaller range

--- src/test/resources/range.sh
+++ src/test/resources/range.sh
@@ -3,7 +3,7 @@ BASE="src/test/java/app/test/"
 PILF='s:\$/(.+)\.(\S+):\2/\1.java:'
 FLIP='s:(acceptance|integration)/(.+).java:$/\2.\1:'
 find "$BASE" -type f | grep -v 'generic' | sed -E "$FLIP" | sort | sed -E "$PILF" | while read SRC; do
-    START=$(grep -nm1 '^//' $SRC | cut -d: -f1)
-    END=$(wc -l $SRC | cut -d\  -f1)
+    START=$(grep -n '^//' $SRC | tail -1 | cut -d: -f1)
+    END=$(grep -n '^}' $SRC | head -1 | cut -d: -f1)
     echo "$SRC $START $END"
 done | column -t | sed -E '/integration/G'
