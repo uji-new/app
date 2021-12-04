8787623bea2c17e4007f8114f8f4ec4f874bc191
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-12-04 18:47:53 +0100

    new test range format

--- src/test/resources/range.sh
+++ src/test/resources/range.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-BASE="src/test/java/app/test/"
+BASE='src/test/java/app/test/'
+ESAB='code/test/'
+
 PILF='s:\$/(.+)\.(\S+):\2/\1.java:'
 FLIP='s:(acceptance|integration)/(.+).java:$/\2.\1:'
+
 find "$BASE" -type f | grep -v 'generic' | sed -E "$FLIP" | sort | sed -E "$PILF" | while read SRC; do
-    START=$(grep -n '^public' $SRC | tail -1 | cut -d: -f1)
-    END=$(grep -n '^}' $SRC | head -1 | cut -d: -f1)
-    echo "$SRC $START $END"
-done | column -t | sed -E '/integration/G'
+    sed "s:^$BASE:$ESAB:" <<< "$SRC"
+    grep -n '^public' $SRC | tail -1 | cut -d: -f1
+    grep -n '^}' $SRC | head -1 | cut -d: -f1
+done
