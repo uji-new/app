190fdfc8d89daa89c9cf166d5e7381beb5a6e3df
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-12-04 18:51:24 +0100

    quote paths

--- src/test/resources/range.sh
+++ src/test/resources/range.sh
@@ -7,6 +7,6 @@ FLIP='s:(acceptance|integration)/(.+).java:$/\2.\1:'
 
 find "$BASE" -type f | grep -v 'generic' | sed -E "$FLIP" | sort | sed -E "$PILF" | while read SRC; do
     sed "s:^$BASE:$ESAB:" <<< "$SRC"
-    grep -n '^public' $SRC | tail -1 | cut -d: -f1
-    grep -n '^}' $SRC | head -1 | cut -d: -f1
+    grep -n '^public' "$SRC" | tail -1 | cut -d: -f1
+    grep -n '^}' "$SRC" | head -1 | cut -d: -f1
 done
