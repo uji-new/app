aa176e4acb4858be65c1dda205a00783107f871f
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2021-12-17 22:17:04 +0100

    duplicate integration tests

--- src/test/java/app/test/acceptance/advanced/requirement02/History01.java
+++ src/test/java/app/test/acceptance/advanced/requirement02/History01.java
@@ -1,40 +1,51 @@
 package app.test.acceptance.advanced.requirement02;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.hasItem;
 
+import org.springframework.http.HttpStatus;
+
 import app.test.generic.SessionTest;
 
-// Como usuario quiero recibir sugerencias de autocompletado correspondientes a lugares para evitar potenciales búsquedas de lugares inexistentes.
+// Como usuario quiero dar de alta una ubicación a partir de un topónimo, con el fin de tenerla disponible en el sistema.
 public class History01 extends SessionTest {
     @Test
     public void valid() {
         // Given
-        var namePartial = "cast";
         var name = "Castellon de la Plana";
+        client.session.loginAsGuest();
+        Mockito.reset(spy.accountManager);
 
         // When
-        var response = client.query.query(namePartial);
+        var response = client.location.addLocation(name);
 
         // Then
+        Mockito.verify(spy.accountManager, never()).saveAccount(any());
         response.statusCode(HttpStatus.OK.value());
-        response.body("", hasSize(1));
-        response.body("name", hasItem(name));
+        var state = client.location.getLocations();
+        state.body("", hasSize(1));
+        state.body("name", hasItem(name));
     }
 
     @Test
     public void invalid() {
         // Given
         var name = "INVALIDO";
+        client.session.loginAsGuest();
+        Mockito.reset(spy.accountManager);
 
         // When
-        var response = client.query.query(name);
+        var response = client.location.addLocation(name);
 
         // Then
-        response.statusCode(HttpStatus.OK.value());
-        response.body("", hasSize(0));
+        Mockito.verify(spy.accountManager, never()).saveAccount(any());
+        response.statusCode(HttpStatus.NOT_FOUND.value());
+        var state = client.location.getLocations();
+        state.body("", hasSize(0));
     }
 }
